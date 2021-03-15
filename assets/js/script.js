1a08dfad847c8c5fb522f78bce9e04b9c7e08fd8
Shy Gois <shayana.gois@att.net>
2021-03-14 18:51:08 -0700

    updated style sheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
 const apiKey = "b565f97a31ef7e75ac1020c5c87fddaa";
-var currentWeatherDiv = $("currentWeather");
-var forecastDiv = $("weatherForecast");
+var currWeatherDiv = $("#currentWeather");
+var forecastDiv = $("#weatherForecast");
 var citiesArray;
+
+if (localStorage.getItem("localWeatherSearches")) {
+    citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches"));
+    writeSearchHistory(citiesArray);
+} else {
+    citiesArray = [];
+};
+
+function returnCurrentWeather(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=${apiKey}`;
+    $.get(queryURL).then(function(response){
+        let currTime = new Date(response.dt*1000);
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        currWeatherDiv.html(`
+        <h2>${response.name}, ${response.sys.country} (${currTime.getMonth()+1}/${currTime.getDate()}/${currTime.getFullYear()})<img src=${weatherIcon} height="70px"></h2>
+        <p>Temperature: ${response.main.temp} &#176;F</p>
+        <p>Humidity: ${response.main.humidity}%</p>
+        <p>Wind Speed: ${response.wind.speed} m/s</p>
+        `, returnUVIndex(response.coord))
+        createHistoryButton(response.name);
+    })
+};
